31ee930026152e581544887f36b98c802f5e9ff4
june050312 <june050312@gmail.com>
2024-07-05 16:19:01 +0900

    feat:image generation

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 
 const app = express();
-const port = 3005;
+const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", require("./Routers/imgGenRouter"));
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
